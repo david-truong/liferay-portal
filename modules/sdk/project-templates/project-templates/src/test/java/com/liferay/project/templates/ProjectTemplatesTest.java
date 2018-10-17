3cbf4ad84726e277492fc96505c6fd406155c7cb
Lawrence Lee <lawrence.lee@liferay.com>
2018-10-17 13:38:52 -0500

    LPS-86370 Use kernel 3.0.0 for 7.1+

--- modules/sdk/project-templates/project-templates/src/test/java/com/liferay/project/templates/ProjectTemplatesTest.java
+++ modules/sdk/project-templates/project-templates/src/test/java/com/liferay/project/templates/ProjectTemplatesTest.java
@@ -4859,7 +4859,7 @@ public class ProjectTemplatesTest {
 		_testContains(
 			gradleProjectDir, "build.gradle",
 			_DEPENDENCY_MODULES_EXTENDER_API + ", version: \"2.0.2",
-			_DEPENDENCY_PORTAL_KERNEL + ", version: \"2.0.0");
+			_DEPENDENCY_PORTAL_KERNEL + ", version: \"3.0.0");
 
 		_testContains(
 			gradleProjectDir, "package.json",
@@ -4947,7 +4947,7 @@ public class ProjectTemplatesTest {
 		_testContains(
 			gradleProjectDir, "build.gradle",
 			_DEPENDENCY_MODULES_EXTENDER_API + ", version: \"2.0.2",
-			_DEPENDENCY_PORTAL_KERNEL + ", version: \"2.0.0");
+			_DEPENDENCY_PORTAL_KERNEL + ", version: \"3.0.0");
 
 		_testContains(
 			gradleProjectDir, "package.json", "@angular/animations",
