63227adbed4519b2cb6fb6625b85614db54c602d
David Truong <david.truong@liferay.com>
2026-04-29 22:56:40 -0500

    LPD-87850 Add integration tests for instant publishing

--- modules/apps/change-tracking/change-tracking-test/src/testIntegration/java/com/liferay/change/tracking/service/test/CTProcessLocalServiceTest.java
+++ modules/apps/change-tracking/change-tracking-test/src/testIntegration/java/com/liferay/change/tracking/service/test/CTProcessLocalServiceTest.java
@@ -6,9 +6,12 @@
 package com.liferay.change.tracking.service.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.change.tracking.constants.CTConstants;
 import com.liferay.change.tracking.model.CTCollection;
+import com.liferay.change.tracking.model.CTPreferences;
 import com.liferay.change.tracking.model.CTProcess;
 import com.liferay.change.tracking.service.CTCollectionLocalService;
+import com.liferay.change.tracking.service.CTPreferencesLocalService;
 import com.liferay.change.tracking.service.CTProcessLocalService;
 import com.liferay.journal.model.JournalFolder;
 import com.liferay.journal.service.JournalFolderLocalService;
@@ -21,6 +24,7 @@ import com.liferay.portal.kernel.change.tracking.CTCollectionThreadLocal;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.service.ClassNameLocalService;
+import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
@@ -29,12 +33,14 @@ import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.test.log.LogCapture;
 import com.liferay.portal.test.log.LogEntry;
 import com.liferay.portal.test.log.LoggerTestUtil;
+import com.liferay.portal.test.rule.FeatureFlag;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
 
 import java.util.List;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
@@ -59,8 +65,180 @@ public class CTProcessLocalServiceTest {
 	public void setUp() throws Exception {
 		_group = GroupTestUtil.addGroup();
 
+		_guestUserId = _userLocalService.getGuestUserId(
+			TestPropsValues.getCompanyId());
+
 		_journalFolderClassNameId = _classNameLocalService.getClassNameId(
 			JournalFolder.class);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		CTPreferences guestCTPreferences =
+			_ctPreferencesLocalService.fetchCTPreferences(
+				TestPropsValues.getCompanyId(), _guestUserId);
+
+		if (guestCTPreferences != null) {
+			guestCTPreferences.setCtCollectionId(
+				CTConstants.CT_COLLECTION_ID_PRODUCTION);
+
+			_ctPreferencesLocalService.updateCTPreferences(guestCTPreferences);
+		}
+	}
+
+	@FeatureFlag("LPD-39203")
+	@Test
+	public void testAddCTProcessWithInstantPublish() throws Exception {
+		CTCollection ctCollection = _addCTCollectionWithContent();
+
+		try (LogCapture ctPreferencesLogCapture =
+				LoggerTestUtil.configureLog4JLogger(
+					"com.liferay.change.tracking.internal.spi.listener." +
+						"CTPreferencesEventListener",
+					LoggerTestUtil.INFO);
+			LogCapture ctProcessLocalServiceImplLogCapture =
+				LoggerTestUtil.configureLog4JLogger(
+					"com.liferay.change.tracking.service.impl." +
+						"CTProcessLocalServiceImpl",
+					LoggerTestUtil.INFO)) {
+
+			CTProcess ctProcess = _ctProcessLocalService.addCTProcess(
+				TestPropsValues.getUserId(), ctCollection.getCtCollectionId());
+
+			List<LogEntry> ctProcessLogEntries =
+				ctProcessLocalServiceImplLogCapture.getLogEntries();
+
+			Assert.assertEquals(
+				ctProcessLogEntries.toString(), 1, ctProcessLogEntries.size());
+
+			LogEntry ctProcessLogEntry = ctProcessLogEntries.get(0);
+
+			Assert.assertEquals(
+				"Using publication " + ctCollection.getName() +
+					" temporarily in place of production",
+				ctProcessLogEntry.getMessage());
+
+			CTPreferences userCTPreferences =
+				_ctPreferencesLocalService.getCTPreferences(
+					TestPropsValues.getCompanyId(),
+					TestPropsValues.getUserId());
+
+			Assert.assertEquals(
+				CTConstants.CT_COLLECTION_ID_PRODUCTION,
+				userCTPreferences.getCtCollectionId());
+
+			BackgroundTask backgroundTask =
+				_backgroundTaskLocalService.getBackgroundTask(
+					ctProcess.getBackgroundTaskId());
+
+			Assert.assertEquals(
+				BackgroundTaskConstants.STATUS_SUCCESSFUL,
+				backgroundTask.getStatus());
+
+			List<LogEntry> ctPreferencesLogEntries =
+				ctPreferencesLogCapture.getLogEntries();
+
+			Assert.assertEquals(
+				ctPreferencesLogEntries.toString(), 1,
+				ctPreferencesLogEntries.size());
+
+			LogEntry ctPreferencesLogEntry = ctPreferencesLogEntries.get(0);
+
+			Assert.assertEquals(
+				"Publication " + ctCollection.getName() +
+					" was published. Production is live.",
+				ctPreferencesLogEntry.getMessage());
+
+			CTPreferences guestCTPreferences =
+				_ctPreferencesLocalService.getCTPreferences(
+					TestPropsValues.getCompanyId(), _guestUserId);
+
+			Assert.assertEquals(
+				CTConstants.CT_COLLECTION_ID_PRODUCTION,
+				guestCTPreferences.getCtCollectionId());
+		}
+	}
+
+	@FeatureFlag("LPD-39203")
+	@Test
+	public void testAddCTProcessWithInstantPublishConflict() throws Exception {
+		CTCollection ctCollection = _addCTCollection();
+
+		String conflictingFolderName = RandomTestUtil.randomString();
+
+		try (SafeCloseable safeCloseable =
+				CTCollectionThreadLocal.setCTCollectionIdWithSafeCloseable(
+					ctCollection.getCtCollectionId())) {
+
+			_journalFolderFixture.addFolder(
+				_group.getGroupId(), conflictingFolderName);
+		}
+
+		_journalFolderFixture.addFolder(
+			_group.getGroupId(), conflictingFolderName);
+
+		try (LogCapture logCapture = LoggerTestUtil.configureLog4JLogger(
+				"com.liferay.portal.background.task.internal.messaging." +
+					"BackgroundTaskMessageListener",
+				LoggerTestUtil.ERROR)) {
+
+			CTProcess ctProcess = _ctProcessLocalService.addCTProcess(
+				TestPropsValues.getUserId(), ctCollection.getCtCollectionId());
+
+			BackgroundTask backgroundTask =
+				_backgroundTaskLocalService.getBackgroundTask(
+					ctProcess.getBackgroundTaskId());
+
+			Assert.assertEquals(
+				BackgroundTaskConstants.STATUS_FAILED,
+				backgroundTask.getStatus());
+
+			List<LogEntry> logEntries = logCapture.getLogEntries();
+
+			Assert.assertEquals(logEntries.toString(), 1, logEntries.size());
+
+			LogEntry logEntry = logEntries.get(0);
+
+			Assert.assertEquals(
+				_UNABLE_TO_EXECUTE_BACKGROUND_TASK, logEntry.getMessage());
+		}
+
+		CTPreferences guestCTPreferences =
+			_ctPreferencesLocalService.getCTPreferences(
+				TestPropsValues.getCompanyId(), _guestUserId);
+
+		Assert.assertEquals(
+			CTConstants.CT_COLLECTION_ID_PRODUCTION,
+			guestCTPreferences.getCtCollectionId());
+	}
+
+	@FeatureFlag("LPD-39203")
+	@Test
+	public void testAddCTProcessWithInstantPublishUserInOtherPublication()
+		throws Exception {
+
+		CTCollection otherCTCollection = _addCTCollection();
+
+		CTPreferences userCTPreferences =
+			_ctPreferencesLocalService.getCTPreferences(
+				TestPropsValues.getCompanyId(), TestPropsValues.getUserId());
+
+		userCTPreferences.setCtCollectionId(
+			otherCTCollection.getCtCollectionId());
+
+		_ctPreferencesLocalService.updateCTPreferences(userCTPreferences);
+
+		CTCollection ctCollection = _addCTCollectionWithContent();
+
+		_ctProcessLocalService.addCTProcess(
+			TestPropsValues.getUserId(), ctCollection.getCtCollectionId());
+
+		userCTPreferences = _ctPreferencesLocalService.getCTPreferences(
+			TestPropsValues.getCompanyId(), TestPropsValues.getUserId());
+
+		Assert.assertEquals(
+			otherCTCollection.getCtCollectionId(),
+			userCTPreferences.getCtCollectionId());
 	}
 
 	@Test
@@ -161,9 +339,32 @@ public class CTProcessLocalServiceTest {
 			LogEntry logEntry = logEntries.get(0);
 
 			Assert.assertEquals(
-				"Unable to execute background task", logEntry.getMessage());
+				_UNABLE_TO_EXECUTE_BACKGROUND_TASK, logEntry.getMessage());
 		}
 	}
+
+	private CTCollection _addCTCollection() throws Exception {
+		return _ctCollectionLocalService.addCTCollection(
+			null, TestPropsValues.getCompanyId(), TestPropsValues.getUserId(),
+			0, RandomTestUtil.randomString(), null);
+	}
+
+	private CTCollection _addCTCollectionWithContent() throws Exception {
+		CTCollection ctCollection = _addCTCollection();
+
+		try (SafeCloseable safeCloseable =
+				CTCollectionThreadLocal.setCTCollectionIdWithSafeCloseable(
+					ctCollection.getCtCollectionId())) {
+
+			_journalFolderFixture.addFolder(
+				_group.getGroupId(), RandomTestUtil.randomString());
+		}
+
+		return ctCollection;
+	}
+
+	private static final String _UNABLE_TO_EXECUTE_BACKGROUND_TASK =
+		"Unable to execute background task";
 
 	@Inject
 	private static JournalFolderLocalService _journalFolderLocalService;
@@ -178,13 +379,20 @@ public class CTProcessLocalServiceTest {
 	private CTCollectionLocalService _ctCollectionLocalService;
 
 	@Inject
+	private CTPreferencesLocalService _ctPreferencesLocalService;
+
+	@Inject
 	private CTProcessLocalService _ctProcessLocalService;
 
 	@DeleteAfterTestRun
 	private Group _group;
 
+	private long _guestUserId;
 	private long _journalFolderClassNameId;
 	private final JournalFolderFixture _journalFolderFixture =
 		new JournalFolderFixture(_journalFolderLocalService);
+
+	@Inject
+	private UserLocalService _userLocalService;
 
 }
