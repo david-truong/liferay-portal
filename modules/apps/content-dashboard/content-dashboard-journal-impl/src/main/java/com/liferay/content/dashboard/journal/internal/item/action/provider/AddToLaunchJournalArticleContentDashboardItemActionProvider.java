3ffa18705e27a32d0e572757a62c49356ab1f031
David Truong <david.truong@liferay.com>
2026-07-20 00:12:55 -0500

    LPD-98970 content-dashboard-journal-impl - Add Draft articles to a launch

---
+++ modules/apps/content-dashboard/content-dashboard-journal-impl/src/main/java/com/liferay/content/dashboard/journal/internal/item/action/provider/AddToLaunchJournalArticleContentDashboardItemActionProvider.java
@@ -0,0 +1,94 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.content.dashboard.journal.internal.item.action.provider;
+
+import com.liferay.content.dashboard.item.action.ContentDashboardItemAction;
+import com.liferay.content.dashboard.item.action.provider.ContentDashboardItemActionProvider;
+import com.liferay.content.dashboard.journal.internal.item.action.AddToLaunchJournalArticleContentDashboardItemAction;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author David Truong
+ */
+@Component(service = ContentDashboardItemActionProvider.class)
+public class AddToLaunchJournalArticleContentDashboardItemActionProvider
+	implements ContentDashboardItemActionProvider<JournalArticle> {
+
+	@Override
+	public ContentDashboardItemAction getContentDashboardItemAction(
+		JournalArticle journalArticle, HttpServletRequest httpServletRequest) {
+
+		if (!isShow(journalArticle, httpServletRequest)) {
+			return null;
+		}
+
+		return new AddToLaunchJournalArticleContentDashboardItemAction(
+			_language);
+	}
+
+	@Override
+	public String getKey() {
+		return "addToLaunch";
+	}
+
+	@Override
+	public ContentDashboardItemAction.Type getType() {
+		return ContentDashboardItemAction.Type.ADD_TO_LAUNCH;
+	}
+
+	@Override
+	public boolean isShow(
+		JournalArticle journalArticle, HttpServletRequest httpServletRequest) {
+
+		if (!journalArticle.isDraft() ||
+			!FeatureFlagManagerUtil.isEnabled(
+				journalArticle.getCompanyId(), "LPD-72278")) {
+
+			return false;
+		}
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		try {
+			return _modelResourcePermission.contains(
+				themeDisplay.getPermissionChecker(), journalArticle,
+				ActionKeys.UPDATE);
+		}
+		catch (PortalException portalException) {
+			_log.error(portalException);
+
+			return false;
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		AddToLaunchJournalArticleContentDashboardItemActionProvider.class);
+
+	@Reference
+	private Language _language;
+
+	@Reference(
+		target = "(model.class.name=com.liferay.journal.model.JournalArticle)"
+	)
+	private ModelResourcePermission<JournalArticle> _modelResourcePermission;
+
+}
